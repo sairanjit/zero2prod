71f9266a5a13eb0367ba8aa84a9601c8c26a35ad
Sai Ranjit Tummalapalli <34263716+sairanjit@users.noreply.github.com>
2022-10-09 12:41:51 +0530

    Chapter 05 (#8)
    * feat: add docker file
    
    * feat: add docker file
    
    * feat: add docker file
    
    * refactor: remove unwanted file

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -53,7 +53,7 @@ async fn spawn_app() -> TestApp {
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // Create database
-    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+    let mut connection = PgConnection::connect(&config.without_db())
         .await
         .expect("Failed to connect to Postgres");
     connection
@@ -61,7 +61,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .await
         .expect("Failed to create database.");
     // Migrate database
-    let connection_pool = PgPool::connect(&config.connection_string())
+    let connection_pool = PgPool::connect(&config.with_db())
         .await
         .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./migrations")
