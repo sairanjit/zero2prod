e4a24f1305723497846c4eaa5e56a75fb9df37b7
Sai Ranjit Tummalapalli <34263716+sairanjit@users.noreply.github.com>
2022-10-09 12:50:50 +0530

    Chapter 05 (#9)
    * feat: add docker file
    
    * feat: add docker file
    
    * feat: add docker file
    
    * refactor: remove unwanted file
    
    * resolve conflicts

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to get configuration");
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(&configuration.database.with_db());
+        .connect_lazy_with(configuration.database.with_db());
 
     let address = format!(
         "{}:{}",
